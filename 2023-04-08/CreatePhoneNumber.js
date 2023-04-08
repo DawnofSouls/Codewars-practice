fdca9947e45090e013a9a3f88ff8b8c966aca6df
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-08 12:25:58 -0700

    Mistakenly created random phone number generator

---
+++ 2023-04-08/CreatePhoneNumber.js
@@ -0,0 +1,11 @@
+//Creates Random Phone Number Function below
+
+/* function createPhoneNumber(numbers){
+    let PN = [];
+    for (let i = 0; i < numbers.length ; i++){
+        PN.push(Math.floor(Math.random() * numbers.length))
+    }
+  return `(${PN[0]}${PN[1]}${PN[2]}) ${PN[3]}${PN[4]}${PN[5]}-${PN[6]}${PN[7]}${PN[8]}${PN[9]}`;
+};
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) */
