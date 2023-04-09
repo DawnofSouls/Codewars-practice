b375ddae95fa64eb9166b1572af935a3fa918579
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-09 14:52:37 -0700

    Counts how many true are in an Array

---
+++ 2023-04-09/CountingSheeps.js
@@ -0,0 +1,9 @@
+function countSheeps(arrayOfSheep) {
+    let Count = 0;
+    array.forEach(element => {
+        if (element === true){
+            Count++
+        }
+    });
+    return Count;
+}
