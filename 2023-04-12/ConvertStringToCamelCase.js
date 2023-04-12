b1b99a8d24dc9b69fabdc08e18a2d068b1fd5942
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-12 14:40:43 -0700

    Converts string to camelCase format

--- 2023-04-12/ConvertStringToCamelCase.js
+++ 2023-04-12/ConvertStringToCamelCase.js
@@ -1,10 +1,16 @@
-function makeNegative(num) {
-  // Code?
-  if (0 > num) {
-    return num;
-  } else {
-    return num * -1;
-  }
+function toCamelCase(str){
+    let Dashed = str.split('-');
+    for (let i = 1; i < Dashed.length; i++){
+        Dashed[i] = Dashed[i].charAt(0).toUpperCase() + Dashed[i].slice(1);
+    };
+    let UnderScore = Dashed.join("").split("_");
+    for (let i = 1; i < UnderScore.length; i++){
+        UnderScore[i] = UnderScore[i].charAt(0).toUpperCase() + UnderScore[i].slice(1);
+    };
+
+    return UnderScore.join("");
 }
 
-console.log(makeNegative(-5));
+console.log(toCamelCase("the_stealth_warrior"));
+
+/* .map(element => element.charAt(0).toUpperCase() + element.slice(1)).join("") */
