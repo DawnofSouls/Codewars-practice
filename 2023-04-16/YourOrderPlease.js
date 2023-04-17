436e41ca17c9e3898ffa39a856f4288c97071ef7
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-16 20:28:54 -0700

    Created function that sorts a string in order that contains number in it

--- 2023-04-16/YourOrderPlease.js
+++ 2023-04-16/YourOrderPlease.js
@@ -1,1 +1,10 @@
-.
+function order(words) {
+  let Array = words.split(" ");
+  let OrderedArray = [];
+    for (let i = 1; i <= Array.length; i++){
+        OrderedArray.push(Array.find(array1 => array1.includes(i)));
+    }
+  return OrderedArray.join(" ");
+}
+
+console.log(order("is2 Thi1s T4est 3a"));
