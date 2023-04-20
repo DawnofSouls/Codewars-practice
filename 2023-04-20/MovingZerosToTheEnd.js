6e7020aad6559515438f5ffde2db6f1526be2c56
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-20 16:14:45 -0700

    Created function that moves Zeros to the end of an array

---
+++ 2023-04-20/MovingZerosToTheEnd.js
@@ -0,0 +1,21 @@
+function moveZeros(arr) {
+    let TotalZeros = 0;
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            TotalZeros++;
+        }
+    }
+    arr = arr.filter(x => !(x === 0));
+
+    for (let i = 0; i < TotalZeros; i++){
+        arr.push(0);
+    }
+    return arr;
+  };
+
+  console.log(moveZeros([ 9, +0, 9, 1, 2, 1, 1, 3, 1, 9, +0, +0, 9, +0, +0, +0, +0, +0, +0, +0 ]));
+  console.log(moveZeros([ 3, false, 5, 1, 1, null, null, '8', null, 6, '1', true, null, '4', false, false, null, '5', '2', +0, [], 4, '5', 2, +0, +0 ]));
+
+  /*     for(let i = 0; i < TotalZeros; i++){
+        arr.push(0);
+    } */
