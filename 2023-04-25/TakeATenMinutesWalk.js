682504e0c09c08c5cbd6bc67f8317765724e3822
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-25 20:10:40 -0700

    Created function to check whether it's a valid 10min walk

---
+++ 2023-04-25/TakeATenMinutesWalk.js
@@ -0,0 +1,17 @@
+function isValidWalk(walk) {
+  //insert brilliant code here
+  function isValidWalk(walk) {
+    let ns = 0, we = 0; 
+      for (let dir of walk) { 
+        if (dir == 'n') ns += 1; 
+        if (dir == 's') ns -= 1; 
+        if (dir == 'w') we += 1; 
+        if (dir == 'e') we -= 1; 
+      } 
+  
+      return walk.length == 10 && ns === 0 && we === 0; 
+  }
+}
+
+  console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']));
+  console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']));
