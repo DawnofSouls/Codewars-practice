34d598da906c0b57483d84dc3b30f345a3a20145
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-27 20:16:35 -0700

    Added vertical and horizontal for loop

--- 2023-04-27/MatrixAddition.js
+++ 2023-04-27/MatrixAddition.js
@@ -1,3 +1,24 @@
 function matrixAddition(a, b){
     //TODO
+    let HorizontalArrayLength = a[0].length;
+    let VerticalLength = a.length;
+
+
+    return a.length;
   }
+
+  console.log(matrixAddition(
+    [ [1, 2],
+      [1, 2] ],
+  //    +
+    [ [2, 3],
+      [2, 3] ] ));
+
+console.log(matrixAddition(
+    [ [1, 2, 3],
+      [3, 2, 1],
+      [1, 1, 1] ],
+  //      +
+    [ [2, 2, 1],
+      [3, 2, 3],
+      [1, 1, 3] ] ));
